54e3c458fec5a8524184af5d63b7920c2a3629f6
osmanjeylani <osmanjeylani00@gmail.com>
2025-08-29 15:29:13 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿List<string> names = [];
+
+Console.Write("Ange ett namn: ");
+names.Add(Console.ReadLine()!);
+
+Console.WriteLine("Namn i listan:");
+foreach (var name in names)
+
+    Console.WriteLine(name);
+
+Console.WriteLine("Tryck på valfri tangent för att avsluta...");
+Console.ReadKey();
