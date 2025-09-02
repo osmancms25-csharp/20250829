5d5797b49cb7695846ff3f5b41e11d3a67e1dbe3
osmanjeylani <osmanjeylani00@gmail.com>
2025-09-02 10:09:33 +0200

    hasse

---
+++ ConsoleApp3/ConsoleApp2/Program.cs
@@ -0,0 +1,12 @@
+﻿int minValue = 0;
+int maxValue = 100;
+int currentValue = 18;
+
+void v = Console.WriteLine("Påståendet är INTE sant";
+
+if (currentValue < maxValue)
+    Console.WriteLine("Påståendet är sant");
+else
+    v;
+
+Console.ReadKey();
---
+++ ConsoleApp4/ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿List<string> name_list = [];
+
+
+
+Console.Clear();
+
+Console.WriteLine("### NAMNLISTA ###");
+
+foreach (string name in name_list)
+{
+    Console.WriteLine(name);
+}
+
+
+Console.ReadKey();
+
+
+string answer = "";
+do
+{
+    Console.WriteLine("Hej och välkommen");
+    Console.Write("Ange ett namn: ");
+
+    string input_name = Console.ReadLine()!;
+
+    if (!string.IsNullOrEmpty(input_name))
+    {
+        name_list.Add(input_name);
+    }
+    Console.Write("Vill du ange ett nytt namna? (y/n): ");
+    answer = Console.ReadLine() !;
+
+}
+while (answer == "y");
+
+{
+
+}
